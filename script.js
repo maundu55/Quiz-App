5c71e019edb5e797e419b89b68454ee1a6611131
maundu55 <marymaundu.mm@gmail.com>
2024-09-09 16:14:59 +0300

    add resetState function

--- script.js
+++ script.js
@@ -61,6 +61,10 @@ function showQuestion(){
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
     })
 }
 
